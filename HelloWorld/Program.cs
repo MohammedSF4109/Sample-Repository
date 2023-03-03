ffdcab48012107791ecbee67b02d902b200dcf36
MohammedSF4109 <mohammed.sf4109@gmail.com>
2023-03-03 12:23:07 +0530

    updated program file

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,9 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello World! {name}");
             Console.ReadLine();
         }
     }
